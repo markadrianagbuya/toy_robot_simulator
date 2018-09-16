0512714133a1e77b8dd3908a1cde478315568f07
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-16 22:10:22 +1000

    ref: Use trig and vector translations to calculate position ahead

--- lib/robot.rb
+++ lib/robot.rb
@@ -8,6 +8,8 @@ class Robot
     "SOUTH" => 270
   }
 
+  DEFAULT_STEP_DISTANCE = 1
+
   def place(position, orientation)
     self.position = position
     self.direction = orientation_to_direction(orientation)
@@ -38,18 +40,16 @@ class Robot
   end
 
   def position_ahead
-    if orientation == "SOUTH"
-      Position.new(x: x_position, y: y_position - 1)
-    elsif orientation == "WEST"
-      Position.new(x: x_position - 1, y: y_position)
-    elsif orientation == "NORTH"
-      Position.new(x: x_position, y: y_position + 1)
-    elsif orientation == "EAST"
-      Position.new(x: x_position + 1, y: y_position)
-    end
+    y_translation = Math::sin(degrees_to_radians(direction)) * DEFAULT_STEP_DISTANCE
+    x_translation = Math::cos(degrees_to_radians(direction)) * DEFAULT_STEP_DISTANCE
+    Position.new(x: x_position + x_translation.round, y: y_position + y_translation.round)
   end
 
   private
+
+  def degrees_to_radians(degrees)
+    degrees * Math::PI / 180
+  end
 
   def set_direction(direction)
     self.direction = direction % 360
