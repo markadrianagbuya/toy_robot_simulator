fe8d499275fa1ccd8388a1d9719196f3a7f16998
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-16 17:40:38 +1000

    test: test that a robot can be placed multiple times

--- spec/toy_robot_simulator_spec.rb
+++ spec/toy_robot_simulator_spec.rb
@@ -29,6 +29,21 @@ RSpec.describe ToyRobotSimulator do
     expect(simulator).to have_received(:puts).with("Output: 3,1,SOUTH")
   end
 
+ it "can accept multiple place commands for the robot" do
+    simulator = ToyRobotSimulator.new
+    simulation_commands = <<~INPUT
+      PLACE 3,1,SOUTH
+      PLACE 2,4,EAST
+      REPORT
+      EXIT
+    INPUT
+    stub_user_inputs(simulator, simulation_commands)
+    allow(simulator).to receive(:puts)
+
+    simulator.start
+    expect(simulator).to have_received(:puts).with("Output: 2,4,EAST")
+  end
+
   def stub_user_inputs(simulator, simulation_commands)
     input_lines = simulation_commands.split("\n").map{ |command| command + "\n" }
     allow(simulator).to receive(:gets).and_return(*input_lines)
