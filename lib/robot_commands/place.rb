df1eeab94bd04765adc5c8f145882aaaa946199b
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-18 08:01:19 +1000

    ref: use regex to capture inputs

--- lib/robot_commands/place.rb
+++ lib/robot_commands/place.rb
@@ -3,12 +3,15 @@ require 'position'
 module RobotCommands
   class Place
     def self.can_handle?(user_command)
-      /PLACE \d,\d,(EAST|NORTH|WEST|SOUTH)/.match?(user_command)
+      place_format.match?(user_command)
+    end
+
+    def self.place_format
+      /PLACE (\d),(\d),(EAST|NORTH|WEST|SOUTH)/
     end
 
     def apply(user_command, robot)
-      _place_command, position_orientation = user_command.split(" ")
-      x, y, orientation = position_orientation.split(",")
+      x, y, orientation = self.class.place_format.match(user_command).captures
 
       robot.place(Position.new(x: x.to_i, y: y.to_i), orientation)
     end
