afbc34701e8387bdf1b0b95a046bcacab6ddf058
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-25 23:36:09 +1000

    test: ensure that robot doesn't move if it would fall off the table

--- spec/robot_commands/move_spec.rb
+++ spec/robot_commands/move_spec.rb
@@ -26,6 +26,14 @@ RSpec.describe RobotCommands::Move do
       expect(robot.y_position).to eq 3
     end
 
+    it "doesn't move if the move would result in falling off table" do
+      robot = Robot.new
+      robot.place(Position.new(x: 0, y: 4), "NORTH")
+      RobotCommands::Move.new.apply("MOVE", robot)
+      expect(robot.x_position).to eq 0
+      expect(robot.y_position).to eq 4
+    end
+
     it "doesn't apply when the robot is not placed" do
       robot = Robot.new
       expect { RobotCommands::Move.new.apply("MOVE", robot) }.to_not raise_error
